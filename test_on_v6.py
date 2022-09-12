f0c966cb5b00e8e176b550b8ccd4c7df0235d0b9
Djura Smits <djura.smits@gmail.com>
2022-09-12 10:28:44 +0200

    Small tweaks to testing script

--- test_on_v6.py
+++ test_on_v6.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
 from time import sleep
-from typing import List
 
 import clize
 import vantage6.client as v6client
 
 from n2n_diagnostics.client import N2NDiagnosticsClient
 
-IMAGE = 'carrrier-harbor.carrier-mu.src.surf-hosted.nl/carrier/n2n-diagnostics'
 RETRY = 10
 SLEEP = 10
 DEFAULT_METHOD = 'echo'
@@ -26,13 +23,15 @@ def test_on_v6(host: str, port: int, username: str, password: str, private_key: 
     active_nodes = client.node.list(is_online=True)
     active_nodes = active_nodes['data']
 
+    print(active_nodes)
+
     org_ids = [n['organization']['id'] for n in active_nodes]
 
-    print(org_ids)
+    print(f' Active nodes{org_ids}')
     master_node = org_ids[0]
     other_nodes = org_ids[1:]
 
-    n2nclient = N2NDiagnosticsClient(client, IMAGE)
+    n2nclient = N2NDiagnosticsClient(client)
     task = n2nclient.echo(master_node, collaboration_id, other_nodes)
 
     print(task)
